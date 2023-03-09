48053b0a7704b4312798fff548bcf2828edee583
WJ_Kim <mw7895la@gmail.com>
2023-03-09 18:07:52 +0900

    [2023-03-09] 1. 임베디드 DB 사용방법

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,18 +2,23 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
+
+@Slf4j
 //@Import(MemoryConfig.class)
 @Import(JdbcTemplateV3Config.class)
-@SpringBootApplication(scanBasePackages = "hello.itemservice.web")	//이 Web이하만 컴포넌트 스캔하고 나머진 수동 등록하겠다 !
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")    //이 Web이하만 컴포넌트 스캔하고 나머진 수동 등록하겠다 !
 public class ItemServiceApplication {
 
-	public static void main(String[] args) {
+	public static void mainm(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
@@ -25,5 +30,18 @@ public class ItemServiceApplication {
 	//빈으로 등록을 해줘야 @EventListener가 동작한다 @Profile은 뭐냐? 로컬pc 운영환경 테스트실행 등 다양한 환경에 따라 다른 설정을 할 때 사용한다.
 	//application.properties에서 spring.profiles.active=local 의 값을 다른걸로 하면 초기 데이터 값이 없다.
 
-
+/*
+	@Bean
+	@Profile("test")
+	public DataSource dataSource(){
+		//여기가 동작하는 경우에는 내가 데이터소스를 직접 등록해서 사용하겠다.
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");	//h2 데이터베이스 드라이버를 지정해준것.
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");		//위 드라이버를 쓰겠다는 것.
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+		//mem 으로 되어있으면 JVM내에 데이터베이스를 만들고 거기에 데이터를 쌓는다.
+	}*/
 }
--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -6,20 +6,38 @@ import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;      //이미 ItemRepository를 구현한 MemoeryItemRepository가 있기 때문에 그게 주입이 될 것.
+
+/*    @Autowired
+    PlatformTransactionManager transactionManager;
+    //DataSource와 PlatformTransactionManager는 스프링 부트가 자동으로 빈으로 등록한다고 했었다.
+    TransactionStatus status;
+
+    @BeforeEach
+    void beforEach() {
+        //트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }*/
 
     @AfterEach
     void afterEach() {
@@ -29,8 +47,12 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        //트랜잭션 롤백
+        //transactionManager.rollback(status);
     }
 
+    //@Commit
     @Test
     void save() {
         //given
@@ -62,6 +84,7 @@ class ItemRepositoryTest {
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
     }
 
+    //여기는 DB에 데이터가 없다고 가정하고 검증하는 부분이라는 것.
     @Test
     void findItems() {
         //given
@@ -75,6 +98,9 @@ class ItemRepositoryTest {
 
         //둘 다 없음 검증
         test(null, null, item1, item2, item3);
+        //이 부분에서 현재 내가 예제 작성하면서 데이터가 많이 쌓여있다. 그런데 Name과 price가 null이니 아마 데이터를 다 가져올 것이다.
+        //그래서 아래 test() 에서 기대한것은 3개일 텐데 너무 많아서 그런것.
+        //즉 이 테스트는 위에 저장한 3개가 디비에 있는지 검증하는 것이었다.
         test("", null, item1, item2, item3);
 
         //itemName 검증
